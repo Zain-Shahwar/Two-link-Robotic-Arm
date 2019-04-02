7a8e5f783fb9c9382ae26f706529ccfb4d85c792
Zain Shahwar <47333843+Zain-Shahwar@users.noreply.github.com>
2019-04-02 12:59:36 +0200

    Dynamics
    Updated

--- Twolink_Dynamics/twolink_symbolic.m
+++ Twolink_Dynamics/twolink_symbolic.m
@@ -2,31 +2,31 @@ clc
 syms q1 q2 q1d q2d q1dd q2dd
 syms a1 a2 g
 syms c1 c2 m1 m2  
-%syms Iyy1 Iyy2 b1 b2
+syms Ixx1 Ixx2 Iyy1 Iyy2 Izz1 Izz2 b1 b2
 Iyy1 = 0;
 Iyy2 = 0;
 b1 = 0;
 b2 = 0;
-% 
+
 twolink = SerialLink([
     Revolute('d', 0, 'a', a1, 'alpha', 0, 'm', m1, 'r', [c1 0 0], 'I', [0 Iyy1 0], 'B', 1.48e-3, 'G', 1, 'Jm', 200e-6, 'standard')
     Revolute('d', 0, 'a', a2, 'alpha', 0, 'm', m2, 'r', [c2 0 0], 'I', [0 Iyy2 0], 'B', 1.48e-3, 'G', 1, 'Jm', 200e-6, 'standard')
     ], ...
     'name', 'R2', ...
-    'comment', 'from Spong, Hutchinson, Vidyasagar');
-twolink = twolink.sym();
-twolink.gravity = [0; 0; g];
-twolink.base = trotx(sym('pi')/2);
-% tau = twolink.rne([q1 q2],[q1d q2d],[q1dd q2dd])
+    'comment', 'from Spong, Hutchinson, Vidyasagar')
+% twolink = twolink.sym();
+% twolink.gravity = [0; 0; g];
+% twolink.base = trotx(sym('pi')/2);
+tau = twolink.rne([q1 q2],[q1d q2d],[q1dd q2dd])
 
-
-inertia = twolink.inertia([q1 q2])
-cor = twolink.coriolis([q1 q2],[q1d q2d]);
-grav = twolink.gravload([q1 q2]);
+% FK = twolink.fkine([q1 q2])
+% inertia = twolink.inertia([q1 q2]);
+% cor = twolink.coriolis([q1 q2],[q1d q2d]);
+% grav = twolink.gravload([q1 q2]);
 % fric = twolink.friction([q1d q2d])
 % twolink.rne([q1 q2],[q1d q2d],[q1dd q2dd])
 % % twolink.inertia([q1 q2])
-% % twolink.dyn
+% twolink.dyn
 % cg = CodeGenerator(twolink);
 % cg.geneverything();
 % L(1) = Link([0 0 0.20 0 0 0]);
